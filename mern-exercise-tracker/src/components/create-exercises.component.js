ef9e612b889eab263bf8ec1bfde282755d68745a
kenakami <kenakami@ucsd.edu>
2020-08-22 01:23:23 -0700

    1

---
+++ mern-exercise-tracker/src/components/create-exercises.component.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class CreateExercises extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <p>You are on the Create Exercises Component!</p>
+            </div>
+        )
+    }
+}
