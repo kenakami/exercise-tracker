001af0cdb66ebc6ea2151bb2c51d427d2819ddb5
kenakami <kenakami@ucsd.edu>
2020-08-22 22:52:24 -0700

    a

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -4,6 +4,9 @@ export default class CreateExercise extends Component {
     constructor(props) {
         super(props);
 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChange
+
         this.state = {
             username: '',
             description: '',
@@ -11,6 +14,45 @@ export default class CreateExercise extends Component {
             date: new Date(),
             users: []
         };
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        window.location = '/';
     }
 
     render() {
